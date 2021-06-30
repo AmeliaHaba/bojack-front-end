0d03cdc01771cfb34f509547b16b80671cebea90
Amelia Haba <amelia.haba347@gmail.com>
2021-06-29 23:09:25 -0400

    update images infofor character card

--- javascript/models/character.js
+++ javascript/models/character.js
@@ -18,6 +18,7 @@ class Character {
         return Character.all.find(character => character.id === parseInt(id))
     }
 
+    
     renderCharacter() {
         const characterCollectionDiv = document.querySelector(".grid-container")
         const characterCardDiv = document.createElement("div")
